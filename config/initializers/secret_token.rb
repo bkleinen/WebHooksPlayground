78ad6743daf646e6b83f6a187e12f4a7e10b18a7
Dr Blinken <drblinken@gmail.com>
2012-12-10 00:33:53 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebHooksPlayground::Application.config.secret_token = 'da29e1857ea3d8c43b555dae656cd904b83ca13486a2bca8e7b8aaa36e03a2fe5cbcefdf02a4a990322396596dabf7c00c385c1c7013dbb4e3c3fe15669fa06c'
