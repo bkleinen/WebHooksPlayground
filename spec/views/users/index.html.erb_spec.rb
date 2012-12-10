79fdc3a8ce6b68ef8032fb4fc3c9be24da3cb3e0
Dr Blinken <drblinken@gmail.com>
2012-12-10 03:43:53 +0100

    fixed pending spec

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -18,7 +18,8 @@ describe "users/index" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Name".to_s, :count => 2
+  end
+  it "does not show token" do
     assert_select "tr>td", :text => "Token".to_s, :count => 0
   end
-  it "does "
 end
